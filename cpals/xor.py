e4bab6c295e4199e521a1cdd6aa10b4f493a3a08
kevin <liguori.km@gmail.com>
2026-09-04 22:16:58 -0400

    complete challenege 02

---
+++ cpals/xor.py
@@ -0,0 +1,11 @@
+from cpals.convert import to_hex, from_hex
+
+
+def fixed_xor(s1, s2):
+    out = []
+    if len(s1) != len(s2):
+        raise ValueError("The buffers must be the same size")
+    b1, b2 = from_hex(s1), from_hex(s2)
+    for i in range(len(b1)):
+        out.append(b1[i] ^ b2[i])
+    return to_hex(out)
