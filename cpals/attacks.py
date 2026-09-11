ef0012ba14f7881914aef3f8353fb1e7c5f4077b
kevin <liguori.km@gmail.com>
2026-09-11 15:07:06 -0400

    added chunking function

--- cpals/attacks.py
+++ cpals/attacks.py
@@ -1,20 +1,25 @@
-from cpals.bits import hamming
+from cpals.bits import hamming, chunking
 
 
-def crack_repeated_key_xor(data: bytes, num_chunks: int = 2) -> bytes:
+def get_key_sizes(data: bytes, num_chunks: int = 2) -> dict[int, float]:
     key_size_dict = {}
     for keysize in range(2, 41):
-        chunk_rng = range(num_chunks)
-        chunks = [data[keysize * i : keysize * (i + 1)] for i in chunk_rng]
+        chunks = chunking(data, keysize, num_chunks)
         hamming_distances = []
-        for i in chunk_rng:
+        for i in range(num_chunks):
             for j in range(i + 1, num_chunks):
                 hamming_distances.append(hamming(chunks[i], chunks[j]))
         avg_hamming = sum(hamming_distances) / len(hamming_distances)
-        key_size_dict[keysize] = round(avg_hamming / keysize, 3)
-    key_size_dict_sorted = dict(sorted(key_size_dict.items(), key=lambda kv: kv[1]))
+        key_size_dict[keysize] = round(avg_hamming / keysize, 2)
+    return dict(sorted(key_size_dict.items(), key=lambda kv: kv[1]))
 
-    for i in range(5):
+
+def crack_repeated_key_xor(
+    data: bytes, num_chunks: int = 2, num_sizes: int = 4
+) -> bytes:
+    keysizes = list(get_key_sizes(data, num_chunks))[:num_sizes]
+
+    for keysize in keysizes:
         pass
 
     return b"00"
--- cpals/bits.py
+++ cpals/bits.py
@@ -2,3 +2,25 @@ def hamming(a: bytes, b: bytes) -> int:
     if len(a) != len(b):
         raise ValueError("Must be the same number of bytes")
     return sum((x ^ y).bit_count() for x, y in zip(a, b))
+
+
+def chunking(data: bytes, size: int, num_chunks: int | None = None) -> list[bytes]:
+    # Check for sane inputs
+    if size <= 0:
+        raise ValueError("size must be a positive integer")
+    if num_chunks is not None and num_chunks <= 0:
+        raise ValueError("num_chunks must be a positive integer or None")
+
+    n = len(data)
+    available_chunks = n // size + int(n % size != 0)
+
+    if num_chunks is None:
+        num_chunks = available_chunks
+    elif num_chunks > available_chunks:
+        raise ValueError("num_chunks exceeds number of available chunks")
+
+    return [data[size * i : size * (i + 1)] for i in range(num_chunks)]
+
+
+def transpose(data: list[bytes]) -> list[bytes]:
+    return [[c[i] for i in range(0, len(c), 
